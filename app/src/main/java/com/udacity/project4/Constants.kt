38b7e57f631d64c65d42d488a7f75f443dcb8fc6
abanoub.samirfahmi@gmail.com <abanoub.samirfahmi@gmail.com>
2022-10-14 17:53:01 +0200

    we implement authentication firbase and add activity in mainfeast.xml and get result

---
+++ app/src/main/java/com/udacity/project4/Constants.kt
@@ -0,0 +1,10 @@
+package com.udacity.project4
+
+import com.google.android.gms.location.Geofence
+
+object Constants {
+
+    const val TAG = "AuthenticationActivity"
+    const val SIGN_IN_RESULT_CODE = 1001   // use to get  result description by result code
+
+}
--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,20 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.Constants
+import com.udacity.project4.locationreminders.RemindersActivity
+
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,6 +22,7 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAuthenticationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
@@ -20,5 +33,64 @@ class AuthenticationActivity : AppCompatActivity() {
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_authentication
+        )
+
+        binding.lifecycleOwner = this
+
+        binding.signInButton.setOnClickListener {
+            launchSign_AUTH()
+        }
     }
+
+    // to use AuthUI  / IdpResponse  classes inside firbase package , we implement
+    // 'com.firebaseui:firebase-ui-auth:7.1.1'  in side gradle project
+    private fun launchSign_AUTH() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            Constants.SIGN_IN_RESULT_CODE
+        )
+    }
+
+    //Starting another activity, whether one within my app or from another app,
+    //  You can also start another activity and receive a result back through on activity result
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Constants.SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(Constants.TAG, "Success >> signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                Toast.makeText(this, "Success >>signed in", Toast.LENGTH_SHORT).show()
+                startReminderActivity()
+
+            } else {
+
+
+                Log.i(Constants.TAG, "Sign in failed ${response?.error?.errorCode}")
+                Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startReminderActivity() {
+        val intent = Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
 }
